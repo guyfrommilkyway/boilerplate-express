c1cf9d0ff3a1a183a35b75012bbc593a498f51ea
Almer Tampus <almerflorestampus@gmail.com>
2024-02-03 17:15:56 +0800

    Setup POST for /name

--- myApp.js
+++ myApp.js
@@ -50,6 +50,8 @@ app
 	.get((req, res) => {
 		res.json({ name: `${req.query.first} ${req.query.last}` });
 	})
-	.post((req, res) => {});
+	.post((req, res) => {
+		res.json({ name: `${req.body.first} ${req.body.last}` });
+	});
 
 module.exports = app;

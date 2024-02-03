620e57bb713518b43729a42a857784f3e90e44cf
Almer Tampus <almerflorestampus@gmail.com>
2024-02-03 17:12:25 +0800

    Mount body-parser

--- myApp.js
+++ myApp.js
@@ -1,10 +1,12 @@
 let express = require('express');
 let app = express();
 require('dotenv').config();
+const bodyParser = require('body-parser');
 
 console.log('Hello World');
 
 app.use('/public', express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/views/index.html');
